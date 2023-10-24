cf6c7f1c25877f451d0b9c38611fcf823a46b316
Evgeniy V <volochek93@yandex.ru>
2023-10-24 16:59:28 +0400

    update

--- Selenium/main.py
+++ Selenium/main.py
@@ -4,18 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://suninjuly.github.io/find_link_text"
+link = "https://63pokupki.ru/auth#/login"
 
 try:
     browser = webdriver.Chrome()
     browser.get(link)
-    link = browser.find_element(By.LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
-    link.click()
-    input1 = browser.find_element(By.TAG_NAME, "input").send_keys("Ivan")
-    input2 = browser.find_element(By.NAME, "last_name").send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, "city").send_keys("Smolensk")
-    input4 = browser.find_element(By.ID, "country").send_keys("Russia")
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+    input1 = browser.find_element(By.CLASS_NAME, "spui-Input__input").send_keys("user1049063")
+    input2 = browser.find_element(By.XPATH, "//input[@type='password']").send_keys("pass")
+    button = browser.find_element(By.CSS_SELECTOR, "button.frm-singIn__btn").click()
 
 finally:
     # закрываем браузер после всех манипуляций
